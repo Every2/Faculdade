efbc0457ad05f33979466ed4410c2b389035b097
christian <christiansantoslima21@gmail.com>
2023-05-06 23:07:28 -0300

    add: atividade feita na aula

---
+++ Atividades/atividade matematica para computação.py
@@ -0,0 +1,11 @@
+"""
+Sabendo que um algoritmo é compreendido como um processo que
+mostra detalhadamente os métodos necessários para a resolução de
+uma atividade, e que podemos pensar nessa estrutura em formato de
+pseudocódigo como estrutura inicial, descreva um pseudocódigo para o
+cálculo da expressão numérica 1 + 3(5 + 1).
+"""
+
+def calculo():
+    print(1 + 3) * (5 + 1)
+
